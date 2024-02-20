f7ec1f3fce30a295b5e787770bd657a9ed74cc7a
Igor <igoralbuquerque021@gmail.com>
2024-02-20 19:09:12 -0300

    chore: connection Mongodb, repository, service

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -4,23 +4,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User user = new User("1", "Maria", "naotem@naotem.com");
-		User use2 = new User("2", "Igor", "naotem@naotem.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(user,use2));
+		
+		List<User> list = service.findAll();
+		
 		return ResponseEntity.ok().body(list);
 		
 	}
